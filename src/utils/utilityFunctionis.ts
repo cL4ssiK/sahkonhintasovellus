3a988ffccc054774c2c1a7d937bc0ada7be0fdf6
Lassi Kupiainen <kupialnx@student.jyu.fi>
2026-05-05 13:28:46 +0300

    alvin lisäys funktio

--- src/utils/utilityFunctionis.ts
+++ src/utils/utilityFunctionis.ts
@@ -4,6 +4,10 @@ export function price_eurMWh_to_sntKWh(price: number): number {
     return price/10;
 }
 
+export function addVAT(price: number): number{
+    return 1.255*price;
+}
+
 export function convertToFixedISOString(date: Date | undefined): string {
     if (!date) return "";
     const offset = date.getTimezoneOffset();
